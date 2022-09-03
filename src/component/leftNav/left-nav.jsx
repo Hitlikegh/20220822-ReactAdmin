8ae66b80c6805d1a48a36b35d0e6b87a359da89a
Hitlikegh <757178180@qq.com>
2022-09-03 23:33:21 +0800

    roleRight

--- src/component/leftNav/left-nav.jsx
+++ src/component/leftNav/left-nav.jsx
@@ -28,8 +28,62 @@ const LeftNav = () => {
     // console.log('path:', path);
 
     const [states, setStates] = useState({
+        menus: [],
         openKeys: ''
     })
+
+    // 得到当前用户的权限数组
+    const getCurrentMenus = () => {
+        const menus = memoryUtils.getUser().role.menus
+        const newMenus = menuList.map((item) => {
+            if (!item.children) {
+
+                return (
+                    menus.includes(item.key) ? {
+                        title: item.title,
+                        key: item.key,
+                        icon: item.icon
+                    } : '0'
+                )
+            }
+            else {
+                const childrenMenus = item.children.map((ic) => {
+                    return (
+                        menus.includes(ic.key) ? {
+                            title: ic.title,
+                            key: ic.key,
+                            icon: ic.icon
+                        } : '0'
+                    )
+                })
+                // // 过滤其中为'0'的值
+                const newChildren = []
+                childrenMenus.forEach(e => {
+                    if (e !== '0') newChildren.push(e)
+                })
+                if (newChildren.length !== 0) {
+                    return (
+                        {
+                            title: item.title,
+                            key: item.key,
+                            icon: item.icon,
+                            children: newChildren
+                        }
+                    )
+                }
+            }
+        })
+        // 过滤其中为'0'和莫名其妙产生的undefined的值
+        const test = []
+        newMenus.forEach(element => {
+            if (element !== '0' && !!element) test.push(element)
+        });
+        setStates({
+            ...states,
+            menus:test
+        })
+    }
+
 
     const getMenuNodes_map = (menuList) => {
 
@@ -83,11 +137,12 @@ const LeftNav = () => {
     };
 
     useEffect(() => {
-        const menuNodes = getMenuNodes_map(menuList);
+        getCurrentMenus();
+        const menuNodes = getMenuNodes_map(states.menus);
 
     }, [])
 
-    const menuNodes = getMenuNodes_map(menuList);
+    const menuNodes = getMenuNodes_map(states.menus);
     let path = location.pathname;
     if (path.indexOf('/product') === 0) {
         path = '/product';
